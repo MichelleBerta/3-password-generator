6f32c6912448601f91570de1006ef107f3455113
Michelle Berta <bertafamily@sbcglobal.net>
2021-08-02 12:06:34 -0500

    wrote character choices

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "-", "_", "+", "=", ":", ";", "<", ">", "?", "/"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,7 +15,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
