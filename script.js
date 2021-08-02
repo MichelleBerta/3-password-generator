51cce3007565d846c371793f8baebee3e8589e2b
Michelle Berta <bertafamily@sbcglobal.net>
2021-08-02 13:50:19 -0500

    wrote functions to get characters from charcode

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
-
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "-", "_", "+", "=", ":", ";", "<", ">", "?", "/"];
-
-// Assignment Code
+// looks for generate button on html //
 var generateBtn = document.querySelector("#generate");
+
+// select characters for password //
+(getRandomLowerCase) => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+(getRandomUpperCase) => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+(getRandomNumbers) => {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+(getRandomSpecialCharacters) => {
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+};
+
+// creates an object to store characters //
+(storeCharacters) = {
+  lower: getRandomLowerCase,
+  upper: getRandomUpperCase,
+  number: getRandomNumbers,
+  special: getRandomSpecialCharacters,
+};
+
+// sets the variable for the generatePassword function //
+
+
+
+
+
+// asks user for desired length of password //
+passwordLength = prompt(
+  "How many characters do you want to include in your password?  Enter a number between 8 - 128.",
+  ""
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
